6ae4c8793f13def0411dccbfb02380e5e1323269
23033ln <23033ln@hvhs.school.nz>
2026-02-10 15:01:05 +1300

    1.8

--- t03_gravity.js
+++ t03_gravity.js
@@ -13,7 +13,7 @@ cnv = new Canvas(1920, 1080);
 
 world.gravity.y = 10;
 
-triangle = new Sprite(100, 100, 80, 90, '90');
+triangle = new Sprite(100, 100, 80, 90, 'd');
 
 triangle.color = 'blue';
 
