5488df2cbc474ac837f3ea205efae8ed1479abe9
23033ln <23033ln@hvhs.school.nz>
2026-02-12 10:45:08 +1300

    3.0

--- t05_createWalls.js
+++ t05_createWalls.js
@@ -9,30 +9,29 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-	cnv = new Canvas(800, 800);
-wallLH  = new Sprite(0, 800/2, 8, 800, 'k');
+    cnv = new Canvas(1000, 1000);
+wallLH  = new Sprite(500, 1000, 100, 1000, 'k');
+wallLH.rotation = 90
 
 wallLH.color = 'yellow';
 
-wallRH  = new Sprite(0, 0, 100, 800, 'k');
+wallRH  = new Sprite(0, 500, 100, 1000, 'k');
 
-wallTop = new Sprite(800, 0, 100, 800, 'k');
+wallRH.color = 'black';
 
-wallBot = new Sprite(x, y, w, h, 'k');
+wallTop = new Sprite(1000, 500, 100, 1000, 'k');
+wallTop.color = 'green';
+world.gravity.y = 10;
 
-
-
-
-
-
+wallBot = new Sprite(500, 0, 100, 1000, 'k');
+wallBot.rotation = 90
 
 ball_1 = new Sprite(width/2, height/2, 50, 'd');
 
 ball_1.color = 'cyan';
 
-ball_1.vel.x = 2;
 
-ball_1.bounciness = 1;
+ball_1.bounciness = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
 
 ball_1.friction = 0;
 
