21123888275d9bf945caaf737d59bab30e834a11
23033ln <23033ln@hvhs.school.nz>
2026-02-12 10:59:44 +1300

    3.1

--- t06_createAliens.js
+++ t06_createAliens.js
@@ -9,14 +9,56 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+    cnv = new Canvas(1000, 1000);
+wallLH  = new Sprite(500, 1000, 100, 1000, 'k');
+wallLH.rotation = 90
+
+wallLH.color = 'yellow';
+
+wallRH  = new Sprite(0, 500, 100, 1000, 'k');
+
+wallRH.color = 'black';
+
+wallTop = new Sprite(1000, 500, 100, 1000, 'k');
+wallTop.color = 'green';
+world.gravity.y = 10;
+
+wallBot = new Sprite(500, 0, 100, 1000, 'k');
+wallBot.rotation = 90
+
+ball_1 = new Sprite(width/2, height/2, 50, 'd');
+
+ball_1.color = 'cyan';
+
+
+ball_1.bounciness = 1;
+ball_1.friction = 0;
+
+ball_1.drag = 0;
+
+cube  = new Sprite(10, 10, 10, 10, 'd');
+
+
+for (i = 0; i < 500; i++) {
+
+  alien = new Sprite(500, 500, 20, 20);
+
+  alien.vel.x =  random(2, 998);
+
+  alien.vel.y = random(2, 998);
+
+  alien.bounciness = 1;
+
+  alien.friction = 0;
+
 
 }
-	
+}
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background ('teal')
 }
 
 /*******************************************************/
